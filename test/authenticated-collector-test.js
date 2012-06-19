81bfa3b2cdf417a84c4a1761e5a18f5aaf3f4154
Trotter Cashion <cashion@gmail.com>
2012-06-18 18:07:19 -0700

    Add test showing how we want authentication to look

---
+++ test/authenticated-collector-test.js
@@ -0,0 +1,35 @@
+var vows   = require("vows"),
+    assert = require("assert"),
+    cube   = require("../"),
+    test   = require("./test");
+
+var suite = vows.describe("collector");
+
+var port = ++test.port, server = cube.server({
+  "mongo-host": "localhost",
+  "mongo-port": 27017,
+  "mongo-database": "cube_test",
+  "http-port": port
+});
+
+server.register = cube.collector.register;
+
+server.start();
+
+suite.addBatch(test.batch({
+  "POST /event/put with valid credentials": {
+    topic: test.request({method: "POST", port: port, path: "/1.0/even/put"}, JSON.stringify([{
+      type: "test",
+      time: new Date,
+      user: "goodUser",
+      password: "goodPassword",
+      data: {
+        foo: "bar"
+      }
+    }])),
+    "responds with status 200": function(response) {
+      assert.equal(response.statusCode, 200);
+      assert.deepEqual(JSON.parse(response.body), {});
+    }
+  }
+}));
